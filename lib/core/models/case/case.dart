4765a830c24026cd78cd78f583c8c4f9894e73d8
Tamir Chuluunbaatar <tamirchuba@gmail.com>
2025-11-12 14:19:52 +0800

    wip: case listing

--- lib/core/models/case/case.dart
+++ lib/core/models/case/case.dart
@@ -1,24 +1,53 @@
+import 'package:intl/intl.dart';
+
 class Case {
   final String title;
+  final String infringementType;
+  final String relatedCountries;
   final List<String> chips;
   final String status;
+  final String progressStatus;
+  final String manager;
   final String date;
   final String id;
+  final String number;
 
   const Case({
     required this.title,
+    required this.infringementType,
+    required this.relatedCountries,
     required this.chips,
     required this.status,
+    required this.progressStatus,
+    required this.manager,
     required this.date,
     required this.id,
+    required this.number,
   });
 
   factory Case.fromJson(Map<String, dynamic> json) {
+    String nameEn = json['creator']?['nameEn']?.toString() ?? '';
+    String nameKr = json['creator']?['nameKr']?.toString() ?? '';
+    String name = '';
+    if (nameEn.trim().isNotEmpty) {
+      name = nameEn;
+    } else if (nameKr.trim().isNotEmpty) {
+      name = nameKr;
+    } else {
+      name = '-';
+    }
+
+    final dateTime = DateTime.parse(json['investigationDate']?.toString() ?? '');
     return Case(
       id: json['id']?.toString() ?? '',
-      title: json['title']?.toString() ?? '',
+      number: json['number']?.toString() ?? '',
+      title: json['caseName']?.toString() ?? '',
+      manager: name,
+      infringementType: json['infringementType']?.toString() ?? '',
+      relatedCountries: json['relatedCountries']?.toString() ?? '',
       status: json['status']?.toString() ?? '',
-      date: json['date']?.toString() ?? '',
+      progressStatus: json['latestRecord']?['progressStatus']?.toString() ?? '',
+      date: DateFormat('yyyy.MM.dd').format(dateTime),
       chips: (json['tags'] as List?)?.map((e) => e.toString()).toList() ?? [],
     );
   }
--- lib/features/cases/presentation/pages/cases_page.dart
+++ lib/features/cases/presentation/pages/cases_page.dart
@@ -15,6 +15,7 @@ class CasesPage extends StatefulWidget {
 
 class _CasesPageState extends State<CasesPage> {
   int _tabIndex = 0; // 0: 전체, 1: 진행중, 2: 종료
+
   @override
   void initState() {
     super.initState();
@@ -29,21 +30,22 @@ class _CasesPageState extends State<CasesPage> {
     final isLoading = caseProvider.loading;
     final error = caseProvider.error;
     final cases = caseProvider.cases;
-    final filtered = (() {
-      switch (_tabIndex) {
-        case 1:
-          return cases.where((e) => e.status == '진행중').toList();
-        case 2:
-          return cases.where((e) => e.status == '종료').toList();
-        default:
-          return cases;
-      }
-    })();
+    // final filtered = (() {
+    //   switch (_tabIndex) {
+    //     case 1:
+    //       return cases.where((e) => e.status == '진행중').toList();
+    //     case 2:
+    //       return cases.where((e) => e.status == '종료').toList();
+    //     default:
+    //       return cases;
+    //   }
+    // })();
     return Scaffold(
       backgroundColor: background,
       appBar: AppBar(
         elevation: 0,
         backgroundColor: background,
+        surfaceTintColor: Colors.transparent,
         shape: const Border(
           bottom: BorderSide(
             color: Color(0xFFDCDCDC),
@@ -54,17 +56,20 @@ class _CasesPageState extends State<CasesPage> {
       ),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(0.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Text(
-                '전체사건 현황',
-                style: TextStyle(fontWeight: FontWeight.w700, fontSize: 24),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+                child: const Text(
+                  '전체사건 현황',
+                  style: TextStyle(fontWeight: FontWeight.w700, fontSize: 24),
+                ),
               ),
               const SizedBox(height: 12),
               Padding(
-                padding: EdgeInsets.zero, // no outer padding
+                padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
                 child: SegmentedTabs(
                   index: _tabIndex,
                   labels: const ['전체', '진행중인 사건', '종료 사건'],
@@ -75,7 +80,7 @@ class _CasesPageState extends State<CasesPage> {
               const SizedBox(height: 12),
               if (error != null)
                 Padding(
-                  padding: const EdgeInsets.all(16),
+                  padding: EdgeInsets.zero,
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
@@ -110,18 +115,18 @@ class _CasesPageState extends State<CasesPage> {
                         return false;
                       },
                       child: ListView.separated(
-                        padding: const EdgeInsets.fromLTRB(12, 4, 12, 12),
-                        itemCount: filtered.length + (isLoading ? 1 : 0),
+                        padding: EdgeInsets.zero,
+                        itemCount: cases.length + (isLoading ? 1 : 0),
                         separatorBuilder: (_, __) => const SizedBox(height: 8),
                         itemBuilder: (context, index) {
-                          if (index == filtered.length) {
+                          if (index == cases.length) {
                             return const Padding(
                               padding: EdgeInsets.all(8.0),
                               child: Center(child: CircularProgressIndicator()),
                             );
                           }
 
-                          final item = filtered[index];
+                          final item = cases[index];
                           return CaseCard(
                             item: item,
                             onTap: () {
